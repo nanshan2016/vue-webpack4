41dba5796f885ec715e6ecd26b25b6ad76d9794a
gaoying <1335029903@qq.com>
2018-11-23 14:45:39 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -46,9 +46,10 @@ module.exports = {
     
     devServer: {
         // 设置主机地址
-        host: 'localhost',
+        host: '0.0.0.0',
         // 设置默认端口
         port: 8011,
+        //https:true,
         // 设置代理
         proxy: {
             '/mock': {
